99233986bd74280564ed0411f66ac83b2b699793
Vivek Ladke <vivek.l@amazatic.com>
2014-06-17 18:19:46 +0530

    updated

--- care_groove/core/views.py
+++ care_groove/core/views.py
@@ -7,5 +7,4 @@ from django.template import RequestContext
 # View for index page
 def home(request):
     context = RequestContext(request)
-    return render_to_response('index.html',{}, context)
-    #return render(request, 'index.html', {'user': request.user})
+    return render(request,'index.html',context)
